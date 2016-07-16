6427ebba520aa9aa7b58f1534b75f8cc04172399
Hidekazu Kobayashi <kobahide789@gmail.com>
2016-07-16 15:21:17 +0900

    make output as xml

--- server.rb
+++ server.rb
@@ -9,24 +9,17 @@ end
 
 get '/' do
   parser = CaboCha::Parser.new
-  tree = parser.parse(params["q"])
+  tree = parser.parse(params["q"].to_s)
   @result = tree.toString(CaboCha::CABOCHA_FORMAT_XML)
+
+  content_type "application/xml", charset: "utf8"
+
   erb :index
 end
 
 __END__
 
 @@index
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>API</title>
-  <style>pre { font: normal 14px/20px courier; }</style>
-</head>
-<body>
-  <pre><code>
-<%= html_safe @result %>
-</code></pre>
-</body>
-</html>
+<?xml version="1.0" encoding="UTF-8"?>
+<%= @result %>
+
